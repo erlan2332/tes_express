7c262cb582625b668ed601c4a98cef8a945a1d0a
erlan <mak@MacBook-Pro-mak.local>
2025-04-09 17:07:34 +0600

    feat: use dynamic API_BASE for HTTP/HTTPS

--- src/config.js
+++ src/config.js
@@ -1,1 +1,5 @@
-export const API_BASE = "http://5.59.233.108:8081"; 
+const isLocalhost = window.location.hostname === "localhost";
+
+export const API_BASE = isLocalhost
+  ? "http://5.59.233.108:8081"
+  : "https://5.59.233.108:8443"; 
